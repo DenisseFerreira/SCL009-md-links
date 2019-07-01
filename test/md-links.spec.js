8beb6b8dd26a57501510f0608f616b7698d4c217
DenisseFerreira <ferreira.denisse@gmail.com>
2019-07-01 15:15:59 -0400

    Instalado eslint

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -14,22 +14,22 @@ describe('mdLinks', () => {
 // // path file y opciones debe recibir mdLinks
 
 
-// //links en un archivo
-// it('Debería retornar 3 links para el archivo prueba.md', async() => {    
-//   await expect(mdlinks.mdLinks('./prueba.md')).resolves.toEqual(
-//     [{href:'https://www.js-y-npm', text:'Node.js y npm', file:'./prueba.md' },
-//     {href: 'https://nejando-la-asincronia-en-javascript/', text:'Asíncronía en js', file:'./prueba.md'},
-//     {href: 'https://docs.npmjs.com/getting-started/what-is-npm',text:'NPM', file:'./prueba.md'}]);
-//  });
+//links en un archivo
+it('Debería retornar 3 links para el archivo prueba.md', async() => {    
+  await expect(mdlinks.mdLinks('./prueba.md')).resolves.toEqual(
+    [{href:'https://www.js-y-npm', text:'Node.js y npm', file:'./prueba.md' },
+    {href: 'https://nejando-la-asincronia-en-javascript/', text:'Asíncronía en js', file:'./prueba.md'},
+    {href: 'https://docs.npmjs.com/getting-started/what-is-npm',text:'NPM', file:'./prueba.md'}]);
+ });
 
 
 
 
  
-// // //stats de un archivo
-// // test("mdLinks, deberia entregar Total Links: 3 Unique Links: 2 para el archivo test-file-1 con stats true",  () =>{
-// //   expect(mdLinks.mdLinks("test/md-files-test/test-file-1.md",{stats:true})).resolves.toEqual("Total Links:"+3+"\n"+"Unique Links:"+2)
-// // });
+// //stats de un archivo
+// test("mdLinks, deberia entregar Total Links: 3 Unique Links: 2 para el archivo test-file-1 con stats true",  () =>{
+//   expect(mdLinks.mdLinks("test/md-files-test/test-file-1.md",{stats:true})).resolves.toEqual("Total Links:"+3+"\n"+"Unique Links:"+2)
+// });
 //  });
 
 //  //validate de un archivo
